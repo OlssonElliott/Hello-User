cb8abeed68edafbe9b47c3dd78e4ada82a69ebcb
Elliott <sendtoelle@hotmail.com>
2024-10-28 13:42:19 +0100

    added member class

---
+++ src/main/java/com/Hello_User/Hello_User/Member.java
@@ -0,0 +1,28 @@
+package com.Hello_User.Hello_User;
+
+public class Member {
+    private String name;
+    private int age;
+
+    Member(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+}
