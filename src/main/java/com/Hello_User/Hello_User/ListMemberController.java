2ce5f30231c2ec7bdd7845c8161ace32b6b1abdc
Elliott <sendtoelle@hotmail.com>
2024-10-28 13:53:19 +0100

    added member list

--- src/main/java/com/Hello_User/Hello_User/ListMemberController.java
+++ src/main/java/com/Hello_User/Hello_User/ListMemberController.java
@@ -1,8 +1,16 @@
 package com.Hello_User.Hello_User;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ListMemberController {
+public static final List<Member> members = new ArrayList<>();
+static{
+    members.add(new Member("Elliott", 32));
+}
+
 
 }
