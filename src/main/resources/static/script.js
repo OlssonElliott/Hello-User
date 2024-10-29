7a1ab581c20461154572e736fc4ad57685be4d81
Elliott <sendtoelle@hotmail.com>
2024-10-29 14:56:58 +0100

    made admin login page, made js file, linked js to all html.

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,9 @@
+document.getElementById("loginBtn").addEventListener("click", () => {
+    let loginName = document.getElementById("name").value;
+    let loginPassword = document.getElementById("password").value;
+    console.log(loginName + " " + loginPassword);
+});
+
+
+
+
