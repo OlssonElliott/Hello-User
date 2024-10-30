246c7d6bb255abaea94ec0f06308c4da3e58cfbd
Elliott <sendtoelle@hotmail.com>
2024-10-30 12:39:42 +0100

    fixed scripts

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,9 +1,17 @@
-document.getElementById("loginBtn").addEventListener("click", () => {
-    let loginName = document.getElementById("name").value;
-    let loginPassword = document.getElementById("password").value;
-    console.log(loginName + " " + loginPassword);
-});
-
-
-
-
+//login knapp, lägger till admin i local storage.
+const loginBtn = document.getElementById("loginBtn");
+    loginBtn.addEventListener("click", () => {
+        let loginName = document.getElementById("name").value;
+        let loginPassword = document.getElementById("password").value;
+        let loginMessage = document.getElementById("loginMessage");
+        console.log("Login attempt:", loginName, loginPassword);
+        
+        if (loginName === "admin" && loginPassword === "admin"){
+            console.log("FUNKAR")
+            localStorage.setItem("loggedIn", "true");
+            loginMessage.textContent = "SUCCESS";
+            displayAdminLoggedIn();
+        }
+        else { loginMessage.textContent = "WRONG USERNAME OR PASSWORD"};
+    });
+    displayAdminLoggedIn();
